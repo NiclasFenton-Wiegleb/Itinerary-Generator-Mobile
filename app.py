089b45c7ea22f8b696442c1e41c08d4fd1757f6c
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-04-28 14:33:19 +0100

    Improving intro text.

--- app.py
+++ app.py
@@ -272,7 +272,7 @@ if st.session_state.button[1] == True:
                     fix_vertical_scroll()
                     st.markdown(f"""## {title_lst[x]}""")  # title
                     st.markdown(f"""### {dataset.name.iloc[stop]}""")  # name
-                    st.image([image_1, image_2], caption= ["", f"""Source: {dataset.img_source.iloc[stop]}"""], width = 300) # images
+                    st.image([image_1, image_2], caption= ["", f"""Source: {dataset.img_source.iloc[stop]}"""], width = 300, use_column_width= True) # images
                     st.write(f"""Address: {dataset.address.iloc[stop]}""")  # address
                     st.write(f"""Link: {dataset.link.iloc[stop]}""") #link to website
 
